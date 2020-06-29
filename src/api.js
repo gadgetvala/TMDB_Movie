c50baaed7f1c80304f05a8156a08ea2070681c53
gadgetvala <surajj.verma7@gmail.com>
2020-06-29 17:32:58 +0530

    Upload

---
+++ src/api.js
@@ -0,0 +1,10 @@
+
+
+
+https://api.themoviedb.org/3/movie/550?api_key=0e1180d5fffd474b7095d883fa0728fe
+
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZTExODBkNWZmZmQ0NzRiNzA5NWQ4ODNmYTA3MjhmZSIsInN1YiI6IjVlZjc2YWQ5MzlhMWE2MDAzNTMyNjQ4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Ue3l3Cp2k9TsfOpCH7Kdn5OPwGK5hkPRsMIUZQ8Yas 
+
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '0e1180d5fffd474b7095d883fa0728fe';
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div>
+			<h1> This page dosent exist </h1>
+		</div>
+	);
+}
+
+export default NotFound;
